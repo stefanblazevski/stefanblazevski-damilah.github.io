8b2f206a3ec1e381b35b87f2fa73b1229edbdeae
Stefan Blazevski <45402103+stefanblazevski@users.noreply.github.com>
2024-06-28 22:03:44 +0200

    initial commit

---
+++ app/js/script.js
@@ -0,0 +1,42 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function(){
+  console.log('click hamburger');
+
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+    
+  }
+  else { // Open Hamburger Menu
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
+
+  }  
+});
+
+// Sticky animation
+
+const primaryHeader = document.querySelector('.header');
+const scrollWatcher = document.createElement('div');
+
+scrollWatcher.setAttribute('data-scroll-watcher', '');
+primaryHeader.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver((entries)=>{
+  primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting);
+}, {rootMargin: ""});
+
+navObserver.observe(scrollWatcher);
